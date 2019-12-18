3db46bcde5a50c8ba8c9e7993266a409baf9688c
aleksandra-ofykp <58667506+aleksandra-ofykp@users.noreply.github.com>
2019-12-18 14:26:29 +0300

    Add files via upload

---
+++ rsa_test.py
@@ -0,0 +1,36 @@
+import unittest
+
+from rsa import rsa,encode_rsa,decode_rsa,evklid,are_relatively_prime
+
+# Tests adapted from `problem-specifications//canonical-data.json` @ v2.0.0
+
+
+class RSATest(unittest.TestCase):
+    def test_relatively_1(self):
+        self.assertEqual(are_relatively_prime(27, 98), True)
+
+    def test_relatively_2(self):
+        self.assertEqual(are_relatively_prime(25, 88), True)
+
+    def test_evklid_1(self):
+        self.assertEqual(evklid(3, 7), (1))
+    
+    def test_evklid_2(self):
+        self.assertEqual(evklid(3, 9), (3))
+
+    # Utility functions
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
+    
+    def test_encode_rsa_encrypt_massage_1(self):
+        self.assertEqual(encode_rsa([899, 11], 60), (308))
+
+    def test_decode_rsa_encrypt_massage_1(self):
+        self.assertEqual(decode_rsa([899, 611], 308), (60))
+
+
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
